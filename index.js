bc9ab74c7a6c0121986f569c47a76fb2e5fd6d65
Sruthi <sruthivs64@gmail.com>
2023-10-09 01:27:54 +0530

    password/confirm password validation

---
+++ index.js
@@ -0,0 +1,23 @@
+var pwd=document.getElementById('password');
+var cPwd=document.getElementById('confirmpassword');
+var alert=document.getElementById('mismatch');
+
+function validate(){
+    var attr='border: 2px solid red;';
+    var attr2='border: 2px solid yellow;';
+if(pwd.value!=cPwd.value){
+    cPwd.setAttribute('style', attr);
+    alert.innerHTML="Password doesn't match";
+}
+else{
+    cPwd.setAttribute('style',attr2);
+    alert.innerHTML="";
+}
+
+}
+
+//if(cPwd){
+//cPwd.addEventListener('focus',validate);
+//}
+
+
